e5c813782d5cdb9774fdcfc88b3cb7da3aa38234
Muhammad Arshad <muhammadarshad@Muhammads-MacBook-Pro-2.local>
2023-10-01 20:06:13 -0700

    done with part 3 hooks

--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -18,6 +18,12 @@ function TableHeader() {
         <tr key={index}>
           <td>{row.name}</td>
           <td>{row.job}</td>
+          <td>
+            <button onClick={() => 
+              props.removeCharacter(index)}>
+              Delete
+            </button>
+          </td>
         </tr>
       );
      }
@@ -29,14 +35,15 @@ function TableHeader() {
      );
   }
 
-  function Table(props) {
+  function Table (props) {
     return (
       <table>
         <TableHeader />
-        <TableBody characterData={props.characterData} />
+        <TableBody characterData={props.characterData} 
+          removeCharacter={props.removeCharacter} />
       </table>
     );
-}
+  }
 
 
 export default Table;
