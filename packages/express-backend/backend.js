d90d06d0f2973f8155f7322c437c9ba9bfec02a1
Muhammad Arshad <muhammadarshad@Muhammads-MacBook-Pro-3.local>
2023-10-16 17:36:07 -0700

    added delete and name/job search

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -19,7 +19,7 @@ const users = {
        { 
           id : 'xyz789',
           name : 'Charlie',
-          job: 'Janitor',
+          job: 'Janitor', 
        },
        {
           id : 'abc123', 
@@ -49,17 +49,28 @@ const findUserByName = (name) => {
         .filter( (user) => user['name'] === name); 
 }
 
- app.get('/users', (req, res) => {
+
+app.get('/users', (req, res) => {
     const name = req.query.name;
-    if (name != undefined){
+    const job = req.query.job;
+
+    if (name!==undefined || job!==undefined) {
         let result = findUserByName(name);
-        result = {users_list: result};
-        res.send(result);
-    }
-    else{
+
+        if (job) {
+            result = result.filter( (user) => user['job'] === job); 
+        }
+
+        if (result.length === 0) {
+            res.status(404).send('No users found matching the criteria.');
+        } else {
+            res.send({ users_list: result});
+        }
+    } else {
         res.send(users);
     }
 });
+
 
 
 const findUserById = (id) =>
@@ -73,5 +84,31 @@ app.get('/users/:id', (req, res) => {
         res.status(404).send('Resource not found.');
     } else {
         res.send(result);
+    }
+});
+
+
+const addUser = (user) => {
+    users['users_list'].push(user);
+    return user;
+}
+
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+});
+
+
+
+app.delete('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send('Resource not found.');
+    } else {
+        const index = users['users_list'].indexOf(result);
+        users['users_list'].splice(index, 1);
+        res.send('User deleted successfully')
     }
 });
